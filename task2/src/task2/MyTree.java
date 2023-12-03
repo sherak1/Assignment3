26d1579055f4a69c1fc2116b2b9f013a4dd63dc4
sherak1 <sherak1@etf.unsa.ba>
2023-12-03 21:38:20 +0100

    [green] empty tree

---
+++ task2/src/task2/MyTree.java
@@ -0,0 +1,16 @@
+package task2;
+
+import org.w3c.dom.Node;
+
+public class MyTree {
+	private Node root;
+
+	public MyTree() {
+		this.root = null;
+	}
+
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+}
