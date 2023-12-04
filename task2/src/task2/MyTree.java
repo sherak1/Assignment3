53542443f7444f39e3ddb6ce464b78ce0a32cb8a
sherak1 <sherak1@etf.unsa.ba>
2023-12-04 03:16:11 +0100

    [green] isSorted

--- task2/src/task2/MyTree.java
+++ task2/src/task2/MyTree.java
@@ -1,7 +1,6 @@
 package task2;
 
-//import org.w3c.dom.Node;
-
+ 
 public class MyTree {
 
 	private Node root;
@@ -61,7 +60,10 @@ public class MyTree {
 			return true;
 		}
 		// If the value we're looking for is less than the current node's data,
-		return value < current.data ? containsRecursive(current.left, value) : containsRecursive(current.right, value);
+		return value == current.data || (value < current.data && containsRecursive(current.left, value))
+		        || (value > current.data && containsRecursive(current.right, value));
+
+		// or [return value < current.data ? containsRecursive(current.left, value) : containsRecursive(current.right, value);
 	}
 
 // remove implementation
@@ -110,7 +112,7 @@ public class MyTree {
 		}
 		return node.data;
 	}
-	//TreeContainsValue implementing
+	// TreeContainsValue implementing
 
 	public boolean contains2(int value) {
 		return contains2Recursive(root, value);
@@ -134,6 +136,27 @@ public class MyTree {
 		else {
 			return contains2Recursive(current.right, value);
 		}
+	}
+
+	public boolean isSorted() {
+		return isSortedRecursive(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+// IsSorted implementing
+	private boolean isSortedRecursive(Node current, int minValue, int maxValue) {
+		// If the current node is null, it's sorted
+		if (current == null) {
+			return true;
+		}
+
+		// the current node's value is within the valid range
+		if (current.data <= minValue || current.data > maxValue) {
+			return false;
+		}
+		// the right subtree with updated minimum value
+		// the left subtree with updated maximum value
+		return isSortedRecursive(current.left, minValue, current.data)
+				&& isSortedRecursive(current.right, current.data, maxValue);
 	}
 
 }
