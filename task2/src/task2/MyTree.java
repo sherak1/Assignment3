8928f80a5cef242bf119e2aa19004e183db8337b
sherak1 <sherak1@etf.unsa.ba>
2023-12-04 01:20:05 +0100

    [green] addValue

--- task2/src/task2/MyTree.java
+++ task2/src/task2/MyTree.java
@@ -1,6 +1,6 @@
 package task2;
 
-import org.w3c.dom.Node;
+//import org.w3c.dom.Node;
 
 public class MyTree {
 	private Node root;
@@ -12,5 +12,58 @@ public class MyTree {
 	public boolean isEmpty() {
 		return root == null;
 	}
+
+	 public void addValue(int value) {
+	        root = addRecursive(root, value);
+	    }
+	 
+	/* public boolean contains(int value) {
+	        return containsRecursive(root, value);
+	    }*/
+
+	    private Node addRecursive(Node current, int value) {
+	        if (current == null) {
+	            return new Node(value);
+	        }
+
+	        if (value < current.data) {
+	            current.left = addRecursive(current.left, value);
+	        } else if (value > current.data) {
+	            current.right = addRecursive(current.right, value);
+	        }
+
+	        return current;
+	    }
+	    public boolean contains(int value) {
+	        return containsRecursive(root, value);
+	    }
+
+	    private boolean containsRecursive(Node current, int value) {
+	        if (current == null) {
+	            return false;
+	        }
+
+	        if (value == current.data) {
+	            return true;
+	        }
+
+	        return value < current.data
+	                ? containsRecursive(current.left, value)
+	                : containsRecursive(current.right, value);
+	    }
+
+	    private static class Node {
+	        int data;
+	        Node left;
+	        Node right;
+
+	        public Node(int data) {
+	            this.data = data;
+	            this.left = null;
+	            this.right = null;
+	        }
+	    }
+
+		 
 
 }
